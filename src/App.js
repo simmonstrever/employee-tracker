e180927ccc5e2a746bdf60f6472382d4acdf42fd
macT <simmonst208@gmail.com>
2020-09-05 20:18:51 -0500

    mvp of employee tracker

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import TableData from './components/Tabledata';
+import Data from './DataSample.json';
+
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      userData: [],
+      editUser: {user: null, display: false}
+    }
+
+  }
+
+  
+  componentWillMount() {
+    
+    if (!localStorage.getItem('userData')) {
+      localStorage.setItem('userData', JSON.stringify(Data))
+    }
+    let data = localStorage.getItem('userData')
+    this.setState({
+      userData: JSON.parse(data)
+    });
+  }
+  
+
+  
+
+  render() {
+
+    return (
+      <div>
+        {/* <Header /> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <TableData 
+                users={this.state.userData}
+                editUser={this.editUser}
+                deleteUser={this.deleteUser} 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
